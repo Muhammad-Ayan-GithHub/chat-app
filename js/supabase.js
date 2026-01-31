3068b26179a5c52deae3dbb284fe601b73a2bd81
Muhammad-Ayan-GithHub <wildflash58@gmail.com>
2026-01-31 11:38:28 +0500

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://nxastrezgmowxpnptvzj.supabase.co",
+  "sb_publishable_TNpzqw2Ar5FD0C2CPv7ysw_ij4RnCxA"
+);
